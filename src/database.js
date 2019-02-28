3b920bc82f01d920e406055a8ca8b9a0843fa2e8
anyone <anyone_dev@yahoo.com>
2019-02-28 11:32:55 -0500

    make it better

--- src/database.js
+++ src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
-mongoose.connect('mongodb://localhost/enanos', { useNewUrlParser: true })
+// mongodb://localhost/enanos
+mongoose.connect('mongodb+srv://anyone:Luglayro1231@cluster0-bav0t.mongodb.net/test?retryWrites=true', { useNewUrlParser: true })
   .then(db => console.log('DB is connected'))
   .catch(err => console.error(err));
